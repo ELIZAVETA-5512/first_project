715efe8d5a2adafca24b320a1e016e8c3bd1e2c2
Elizaveta <Elizaveta-5512@mail.ru>
2024-03-25 16:16:03 +0300

    main2

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
+
+from phones.models import Phone
 
 
 def index(request):
@@ -7,11 +9,20 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    phones = Phone.objects.all()
+    sort_by = request.GET.get('sort')
+    if sort_by == 'name':
+        phones = phones.order_by('name')
+    elif sort_by == 'min_price':
+        phones = phones.order_by('price')
+    elif sort_by == 'max_price':
+        phones = phones.order_by('-price')
+    context = {'phones': phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.get(slug=slug)
+    context = {'phone': phone}
     return render(request, template, context)
